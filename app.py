e5c98a517e9f87ffa50a6c92673f386c741f70dc
Claudia Lam <claudiaslam@gmail.com>
2023-03-20 12:45:31 -0700

    add api route for cupcakes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, Cupcake, db
@@ -18,3 +18,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", "postgresql:///cupcakes")
 
 connect_db(app)
+
+@app.get('/api/cupcakes')
+def get_cupcake_data():
+    """returns JSON object of cupcakes
+
+    {cupcakes: [{id, flavor, size, rating, image}, ...]}
+
+    """
+
+    #get all data from database
+    cupcakes = Cupcake.query.all()
+    #serialize our cupcakes data
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+    #return our cupcakes data in json
+    return jsonify(cupcakes=serialized)
--- models.py
+++ models.py
@@ -40,6 +40,18 @@ class Cupcake(db.Model):
         nullable=False,
         default='https://tinyurl.com/demo-cupcake')
 
+    def serialize(self):
+
+        """ serialize to dictionary.  """
+
+        return {
+            "id" : self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image,
+        }
+
 
 
 
