14010ad3ed4c2698a48538ddafb36a4c7a7b56c0
Evan Hesketh <evanhesketh@yahoo.ca>
2023-03-20 15:15:32 -0400

    complete part 1

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, Cupcake, db
+
+# from forms import AddPetForm, EditPetForm
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+
+connect_db(app)
--- models.py
+++ models.py
@@ -1,1 +1,47 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Model for handling cupcake info."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    flavor = db.Column(
+        db.Text,
+        nullable=False)
+
+    size = db.Column(
+        db.Text,
+        nullable=False)
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False)
+
+    image = db.Column(
+        db.Text,
+        nullable=False,
+        default='https://tinyurl.com/demo-cupcake')
+
+
+
+
+
+
